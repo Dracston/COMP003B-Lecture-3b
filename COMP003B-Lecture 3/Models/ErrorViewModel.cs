8c320aaef34ccc1b60e435daf82c5180e6ac25c8
Dracston <lpj11101@gmail.com>
2024-04-08 18:18:38 -0700

    Add project files.

---
+++ COMP003B-Lecture 3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B_Lecture_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
